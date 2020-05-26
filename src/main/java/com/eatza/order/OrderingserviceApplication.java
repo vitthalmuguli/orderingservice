968d6bc13418f0c3c73096425e9ac21f2345bfe5
vitthal baburao <Vitthal.Baburao@Staples.com>
2020-05-26 16:49:56 +0530

    first commit

---
+++ src/main/java/com/eatza/order/OrderingserviceApplication.java
@@ -0,0 +1,30 @@
+package com.eatza.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.Bean;
+
+import com.eatza.order.config.JwtFilter;
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableCircuitBreaker      
+public class OrderingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderingserviceApplication.class, args);
+	}
+	
+	@Bean
+	public FilterRegistrationBean jwtFilterBean() {
+		final FilterRegistrationBean registrationBean = new FilterRegistrationBean();
+		registrationBean.setFilter(new JwtFilter());
+		registrationBean.addUrlPatterns("/order/*");
+
+		return registrationBean;
+	}
+
+}
---
+++ src/test/java/com/eatza/order/service/OrderingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eatza.order.service;
+//package com.eatza.order;
+//
+//import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class OrderingserviceApplicationTests {
+//
+//	
+//}
